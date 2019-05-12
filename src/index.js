44134ad8e6b70393ec12ff2bdc1ce396048e1f6e
donghwipark <donghwipark@gmail.com>
2019-05-12 21:15:27 +0900

    basic router modified

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-const hello = "hello world"
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+// setting router
+router.get('/', (ctx) => {
+    ctx.body = 'Home'
+})
+
+router.get('/about/:name?', (ctx) => {
+    const {name} = ctx.params
+    // print depend on the name
+    ctx.body = name ? `Introducing ${name}` : "introducing"
+})
+
+router.get('/posts', (ctx) => {
+    const {id} = ctx.query
+    // print depend on the id
+    ctx.body = id ? `Post #${id}` : "No Post ID"
+})
+
+// apply router in app Instance
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(4000, () => {
+    console.log('listening to port 4000')
+})
