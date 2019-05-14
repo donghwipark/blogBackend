9d2cc85e541ca639c7c11f88dc4b5ba1d885e42c
donghwipark <donghwipark@gmail.com>
2019-05-14 20:21:01 +0900

    Done with pagination function in posts

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -50,9 +50,31 @@ exports.write = async (ctx) => {
 }
 
 exports.list = async (ctx) => {
+    // If there is no page, reply with 1
+    // Change to number from string
+    const page = parseInt(ctx.query.page || 1, 10)
+
+    // On page mistake
+    if(page < 1) {
+        ctx.status = 400
+        return
+    }
     try {
-        const posts = await Post.find().exec()
-        ctx.body = posts
+        const posts = await Post.find()
+            .sort({_id: -1})
+            .limit(10)
+            .skip((page - 1) * 10)
+            .lean()
+            .exec()
+        const postCount = await Post.countDocuments().exec()
+        // tell last page
+        // Set ctx.set on response header
+        const limitBodyLength = post => ({
+            ...post.toJSON(),
+            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+        })
+        ctx.set('Last-Page', Math.ceil(postCount / 10))
+        ctx.body = posts.map(limitBodyLength)
     } catch(e) {
         ctx.throw(e, 500)
     }
